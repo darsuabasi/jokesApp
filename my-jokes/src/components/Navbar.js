6d8cfee73bae0dc5fe4c48a10f29e60f7803dd2a
darsuabasi <abasibasiurua@gmail.com>
2021-03-30 06:27:55 -0400

    create basic navbar + styling

---
+++ my-jokes/src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import './css/Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className="navbar-main-div">
+            <div className="navbar-sub-div">
+                
+                <div class="navbar-logo">
+                    UEA-DJ
+                </div>
+
+                <div className="menu-div">
+                    <ul navbar-main-ul>
+                        <li className="nav-item">
+                            <NavLink className="navlink-style"  exact to="/">
+                                Home
+                            </NavLink>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Navbar
