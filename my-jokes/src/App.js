fccdf3cc88e3a797d696af78a9f628b0f7786146
darsuabasi <abasibasiurua@gmail.com>
2021-03-31 04:47:10 -0400

    update app/client with all routing

--- my-jokes/src/App.js
+++ my-jokes/src/App.js
@@ -2,8 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
+import Dashboard from './components/Dashboard';
 import Navbar from './components/Navbar';
-
+import RandomJokes from './components/Jokes/RandomJokes';
+import ImageJokes from './components/Jokes/ImageJokes';
+import RandomJokeSlack from './components/Jokes/RandomJokeSlack';
+import SearchJokes from './components/Jokes/SearchJokes';
+import JokeResults from './components/Jokes/JokeResults';
+import imageData from './data/imageData.js';
 
 const App = () => {
   return (
@@ -18,7 +24,30 @@ const App = () => {
               <Navbar/> 
 
               <Switch>
-                
+                <Route exact path={"/dashboard"}>
+                  {/* dont need to pass ass prop anymore since adding manually due to 3 cubes*/}
+                  <Dashboard dadImages={imageData}/>
+                </Route>
+
+                <Route exact path={"/dashboard/random-jokes"}>
+                  <RandomJokes/>
+                </Route>
+
+                {/* <Route exact path={"/dashboard/image-jokes"}>
+                  <ImageJokes/>
+                </Route> */}
+
+                {/* <Route exact path={"/dashboard/slack-random-jokes"}>
+                  <RandomJokeSlack/>
+                </Route> */}
+
+                <Route exact path={"/dashboard/search-jokes"}>
+                  <SearchJokes/>
+                </Route>
+
+                <Route exact path="/dashboard/search-jokes/results/:sessionStorage">
+                  <JokeResults/>
+                </Route>
               </Switch>
             </div>
         </Switch>
