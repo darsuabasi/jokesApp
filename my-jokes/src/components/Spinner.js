ecd21eccc9b08e8bd6672ecef5eb94f19fff062b
darsuabasi <abasibasiurua@gmail.com>
2021-04-02 03:45:43 -0400

    create spinner

---
+++ my-jokes/src/components/Spinner.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './css/Spinner.css';
+
+const Spinner = () => {
+    return (
+        <section className="spinner-section">
+            <div className="spinner-loader">
+                <span id="--i:1;"> </span>
+                <span id="--i:2;"> </span>
+                <span id="--i:3;"> </span>
+                <span id="--i:4;"> </span>
+                <span id="--i:5;"> </span>
+                <span id="--i:6;"> </span>
+                <span id="--i:7;"> </span>
+                <span id="--i:8;"> </span>
+                <span id="--i:9;"> </span>
+                <span id="--i:10;"> </span>
+                <span id="--i:11;"> </span>
+                <span id="--i:12;"> </span>
+                <span id="--i:13;"> </span>
+                <span id="--i:14;"> </span>
+                <span id="--i:15;"> </span>
+                <span id="--i:16;"> </span>
+                <span id="--i:17;"> </span>
+                <span id="--i:18;"> </span>
+                <span id="--i:19;"> </span>
+                <span id="--i:20;"> </span>
+            </div>
+        </section>
+    )
+
+}
+
+export default Spinner;
